2d9b1cd4067319e420ebcf5645a4984dd7f3961a
RLA0466 <ruchandani.lalit@external.toyota-europe.com>
2019-01-21 08:58:56 +0530

    - Add missing modules  - externalized config for DB through environment variable inside docker compose

--- book-service/src/main/java/com/lalit/library/BookServiceApplication.java
+++ book-service/src/main/java/com/lalit/library/BookServiceApplication.java
@@ -1,17 +1,39 @@
 package com.lalit.library;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BookServiceApplication {
 
+  private static final Logger LOG = getLogger(BookServiceApplication.class);
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
+
+	@Value("${spring.datasource.url}")
+	String datasourceurl;
+
+	@Bean
+	CommandLineRunner commandLineRunner(){
+    return (arg) -> {
+      LOG.info("Datasource url :" + datasourceurl);
+      System.out.println("Datasource url :" + datasourceurl);
+    };
+	}
+
+
 
 }
 
